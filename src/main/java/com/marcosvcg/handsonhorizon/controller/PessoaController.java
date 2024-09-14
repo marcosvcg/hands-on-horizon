b966f0f28f744e7aa0593969d08b7130a36a1fb2
marcosvcg <marcos.galvao@ucsal.edu.br>
2024-09-14 14:40:15 -0300

    "FEAT: adiciona PessoaController"

---
+++ src/main/java/com/marcosvcg/handsonhorizon/controller/PessoaController.java
@@ -0,0 +1,41 @@
+package com.marcosvcg.handsonhorizon.controller;
+
+import com.marcosvcg.handsonhorizon.model.dto.PessoaDTO;
+import com.marcosvcg.handsonhorizon.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    private final PessoaService pessoaService;
+
+    @Autowired
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    @GetMapping
+    public List<PessoaDTO> getPessoas() {
+        return pessoaService.getPessoas();
+    }
+
+    @GetMapping("/{pessoaId}")
+    public PessoaDTO getPessoaById(@PathVariable("pessoaId") UUID id) {
+        return pessoaService.getPessoaByID(id);
+    }
+
+    @GetMapping("/{nome}")
+    public PessoaDTO getPessoaByNome(@PathVariable("nome") String nome) {
+        return pessoaService.getPessoaByNome(nome);
+    }
+
+    @PostMapping
+    public void createPessoa(@RequestBody PessoaDTO pessoaDTO) {
+        pessoaService.createPessoa(pessoaDTO);
+    }
+}
